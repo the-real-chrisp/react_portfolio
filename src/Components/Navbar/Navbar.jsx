51f09a39091a35b969a33b4b784411650d33247d
Chris Purcell <chrisp0000@gmail.com>
2025-02-14 12:25:17 -0800

    added logo to Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../assets/logo.svg'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      
+      <img src={logo} alt="Logo Dev w/Chris" />
     </div>
   )
 }
