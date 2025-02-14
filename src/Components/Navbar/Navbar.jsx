827629a9c930822719cffb86474e3cc9444cde9a
Chris Purcell <chrisp0000@gmail.com>
2025-02-14 12:31:33 -0800

    added unordered list for navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,6 +6,14 @@ const Navbar = () => {
   return (
     <div className='navbar'>
       <img src={logo} alt="Logo Dev w/Chris" />
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>About Me</li>
+        <li>Services</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+      </ul>
+      <div className="nav-connect">Connect With Me</div>
     </div>
   )
 }
