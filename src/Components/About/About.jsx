5d397d63341d50450aafea06697efe9e9fd97e3a
Chris Purcell <chrisp0000@gmail.com>
2025-02-14 14:06:18 -0800

    mounted about to app and added html for about section

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -3,8 +3,28 @@ import './About.css'
 
 const About = () => {
   return (
-    <div>
-      
+    <div className='about'>
+      <div className="about-title">
+        <h1>About Me</h1>
+
+      </div>
+      <div className="about-sections">
+        <div className="about-left">
+
+        </div>
+        <div className="about-right">
+            <div className="about-para">
+                <p></p>
+                <p></p>
+            </div>
+            <div className="about-skills">
+                <div className="about-skill"><p>HTML & CSS</p><hr style={{width:"90%"}}/></div>
+                <div className="about-skill"><p>React JS</p><hr style={{width:"60%"}}/></div>
+                <div className="about-skill"><p>JavaScript</p><hr style={{width:"80%"}}/></div>
+                <div className="about-skill"><p>Node JS</p><hr style={{width:"70%"}}/></div>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
