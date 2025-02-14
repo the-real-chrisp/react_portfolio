d7e861c51271c63fd9f72f2b8943f365f27686ed
Chris Purcell <chrisp0000@gmail.com>
2025-02-14 13:15:42 -0800

    added new profile pic and hero component plus img and descriptions

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Hero.css'
+import hero_image from '../../assets/20240927_124816.jpg'
 
 const Hero = () => {
   return (
-    <div className='Hero'>
-      
+    <div className='hero'>
+      <img src={hero_image} alt="Hero image displaying photo of website owner" />
+      <h1>I'm Chris Purcell, software developer based in the US.</h1>
+      <p>I'm a front-end and back-end developer from California ready to help you build your dream web application!</p>
+      <div className="hero-action">
+        <div className="hero-connect">Connect With Me</div>
+        <div className="hero-resume">My Resume</div>
+      </div>
     </div>
   )
 }
