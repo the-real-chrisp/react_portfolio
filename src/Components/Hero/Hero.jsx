09dde27525c7b9e1c8dc7f32632636f54785992e
Chris Purcell <chrisp0000@gmail.com>
2025-02-14 13:45:15 -0800

    added styling for hero

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Hero.css'
-import hero_image from '../../assets/20240927_124816.jpg'
+import hero_image from '../../assets/profile_pic_react_portfolio.jpg'
 
 const Hero = () => {
   return (
     <div className='hero'>
-      <img src={hero_image} alt="Hero image displaying photo of website owner" />
-      <h1>I'm Chris Purcell, software developer based in the US.</h1>
+      <img className='hero-img' src={hero_image} alt="Hero image displaying photo of website owner" />
+      <h1><span>I'm Chris Purcell,</span> software developer based in the US.</h1>
       <p>I'm a front-end and back-end developer from California ready to help you build your dream web application!</p>
       <div className="hero-action">
         <div className="hero-connect">Connect With Me</div>
