f1d110f75a17d8aa43be6fa8c18557ce386214d7
Chris Purcell <chrisp0000@gmail.com>
2025-02-14 16:04:27 -0800

    added services data to object and imported to Services.jsx. Mapped to display each object and data.

---
+++ src/assets/service_data.js
@@ -0,0 +1,34 @@
+const Service_Data = [
+    {
+        s_no: '01',
+        s_name: 'Front-End Development',
+        s_desc: 'Building responsive, user-friendly web interfaces using HTML, CSS, JavaScript, and React.'
+    },
+    {
+        s_no: '02',
+        s_name: 'Back-End Development',
+        s_desc: 'Developing and maintaining server-side applications with Node.js and Express.' 
+    },
+    {
+        s_no: '03',
+        s_name: 'Database Design and Optimization',
+        s_desc: 'Designing efficient database schemas and optimizing data retrieval performance.'
+    },
+    {
+        s_no: '04',
+        s_name: 'Project Management',
+        s_desc: 'Leading and managing projects from planning to delivery, ensuring deadlines and criteria are met.'
+    },
+    {
+        s_no: '05',
+        s_name: 'Version Control & Continuous Integration',
+        s_desc: 'Implementing best practices for version control and CI/CD pipelines using GitHub and Git Actions.'
+    },
+    {
+        s_no: '06',
+        s_name: 'Mentorship & Team Collaboration',
+        s_desc: 'Coaching peers on development best practices and helping teams collaborate effectively.'
+    }
+]
+
+export default Service_Data;
