1a00484fae31dc5659262eee21d9ebfa5068d3aa
Chris Purcell <chrisp0000@gmail.com>
2025-02-16 13:46:30 -0800

    Created MyWork component and added pics for my work to assets

---
+++ src/Components/MyWork/MyWork.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './MyWork.css'
+
+const MyWork = () => {
+  return (
+    <div className='mywork'>
+      <div className="mywork-title">
+        <h1>My Latest Work</h1>
+      </div>
+      <div className="mywork-container">
+        
+      </div>
+    </div>
+  )
+}
+
+export default MyWork
---
+++ src/assets/mywork_data.js
@@ -0,0 +1,51 @@
+import project_img_1 from '../assets/EliteMotion.png'
+import project_img_2 from '../assets/Workday_Planner.jpg'
+import project_img_3 from '../assets/lunchRoulette.png'
+import project_img_4 from '../assets/Just-Notes.png'
+import project_img_5 from '../assets/digital_clock.jpg'
+import project_img_6 from '../assets/Random_password.jpg'
+import project_img_7 from '../assets/SQL_database.png'
+import project_img_8 from '../assets/API_ecommerce.png'
+
+const mywork_data = [
+    {
+        work_no: 1,
+        work_name: 'Full-Stack App: EliteMotion',
+        work_img: project_img_1
+    },
+    {
+        work_no: 2,
+        work_name: 'Front-End jQuery Workday Planner',
+        work_img: project_img_2
+    },
+    {
+        work_no: 3,
+        work_name: 'Front-End Lunch Generator',
+        work_img: project_img_3
+    },
+    {
+        work_no: 4,
+        work_name: 'Express and Node Note App',
+        work_img: project_img_4
+    },
+    {
+        work_no: 5,
+        work_name: 'Front-End Digital Clock',
+        work_img: project_img_5
+    },
+    {
+        work_no: 6,
+        work_name: 'JavaScript Password Generator',
+        work_img: project_img_6
+    },
+    {
+        work_no: 7,
+        work_name: 'MySQL HR Tracker Database',
+        work_img: project_img_7
+    },
+    {
+        work_no: 8,
+        work_name: 'Back-End E-commerce',
+        work_img: project_img_8
+    }
+]
