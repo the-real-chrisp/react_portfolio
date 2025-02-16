6da1b8139135b5ee56ff2c6a195e7ccca9fdba66
Chris Purcell <chrisp0000@gmail.com>
2025-02-16 14:43:34 -0800

    Added styling for MyWork section and inlcuded name for work

--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -3,21 +3,29 @@ import './MyWork.css'
 import mywork_data from '../../assets/mywork_data.js'
 
 const MyWork = () => {
-  return (
-    <div className='mywork'>
-      <div className="mywork-title">
-        <h1>My Latest Work</h1>
-      </div>
-      <div className="mywork-container">
-        {mywork_data.map((work, index) => {
-            return <img key={index} src= {work.work_img} alt="" />
-        })}
-      </div>
-      <div className="mywork-showmore">
-        <p>Show More</p>
-      </div>
-    </div>
-  )
+    return (
+        <div className='mywork'>
+            <div className="mywork-title">
+                <h1>My Latest Work</h1>
+            </div>
+            <div className="mywork-container">
+                {/* {mywork_data.map((work, index) => {
+                    return <img key={index} src={work.work_img} alt="" />
+                    
+                })} */}
+                {mywork_data.map((work, index) => {
+                    return <div key={index} className="work-format">
+                        {/* <h3>{work.work_no}</h3> */}
+                        <h2>{work.work_name}</h2>
+                        <img key={index} src={work.work_img} alt="" />
+                    </div>
+                })}
+            </div>
+            <div className="mywork-showmore">
+                <p>Show More</p>
+            </div>
+        </div>
+    )
 }
 
 export default MyWork
