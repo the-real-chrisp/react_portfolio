d5e4b4cea15d3d7dcb548ad0c78638010ff4538f
Chris Purcell <chrisp0000@gmail.com>
2025-02-16 16:33:45 -0800

    added Footer component with copyright

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>©2025 Christopher Purcell. All rights reserved. MIT License</p>
+    </div>
+  )
+}
+
+export default Footer
