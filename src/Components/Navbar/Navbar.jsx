ff1e2c16ba903f493d4bdf6ad8ffb4fcc34c3b96
Chris Purcell <chrisp0000@gmail.com>
2025-02-16 20:00:20 -0800

    Added reference for open and close of menu

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.svg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
@@ -6,12 +6,23 @@ import menu_open from '../../assets/dropdown_menu.svg'
 import menu_close from '../../assets/dropdown_close.svg'
 
 const Navbar = () => {
+
+  const menuRef = useRef();
+
+  const openMenu = () => {
+    menuRef.current.style.right = "0"
+  }
+
+  const closeMenu = () => {
+    menuRef.current.style.right = "-350px"
+  }
+
   return (
     <div className='navbar'>
-      <img src={logo} alt="Logo Dev w/Chris" />
-      <img src={menu_open} alt="" className='nav-mob-open'/>
-      <ul className="nav-menu">
-        <img src={menu_close} alt="" className="nav-mob-close" />
+      <img id='logo-img' src={logo} alt="Logo Dev w/Chris" />
+      <img src={menu_open} onClick={openMenu} alt="" className='nav-mob-open'/>
+      <ul ref={menuRef} className="nav-menu">
+        <img src={menu_close} onClick={closeMenu} alt="" className="nav-mob-close" />
         <li><AnchorLink className='anchor-link' offset={50} href='#home'><p>Home</p></AnchorLink></li>
         <li><AnchorLink className='anchor-link' offset={50} href='#about'><p>About Me</p></AnchorLink></li>
         <li><AnchorLink className='anchor-link' offset={50} href='#services'><p>Services</p></AnchorLink></li>
