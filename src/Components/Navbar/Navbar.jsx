eb8f5794bda7cace55e1824b3217ce0a68d8dd23
Chris Purcell <chrisp0000@gmail.com>
2025-02-16 18:06:00 -0800

    Added svgt for menu and styled media query for responsive design

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.svg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import menu_open from '../../assets/dropdown_menu.svg'
+import menu_close from '../../assets/dropdown_close.svg'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
       <img src={logo} alt="Logo Dev w/Chris" />
+      <img src={menu_open} alt="" className='nav-mob-open'/>
       <ul className="nav-menu">
+        <img src={menu_close} alt="" className="nav-mob-close" />
         <li><AnchorLink className='anchor-link' offset={50} href='#home'><p>Home</p></AnchorLink></li>
         <li><AnchorLink className='anchor-link' offset={50} href='#about'><p>About Me</p></AnchorLink></li>
         <li><AnchorLink className='anchor-link' offset={50} href='#services'><p>Services</p></AnchorLink></li>
