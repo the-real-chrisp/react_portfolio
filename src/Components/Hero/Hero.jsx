e7fbba8ab2a9b53fea09b752ea92f081c7307914
Chris Purcell <chrisp0000@gmail.com>
2025-02-16 17:16:58 -0800

    Added scroll functionality to navbar and contact buttons

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Hero.css'
 import hero_image from '../../assets/profile_pic_react_portfolio.jpg'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Hero = () => {
   return (
@@ -9,7 +10,7 @@ const Hero = () => {
       <h1><span>I'm Chris Purcell,</span> software developer based in the US.</h1>
       <p>I'm a front-end and back-end developer from California ready to help you build your dream web application!</p>
       <div className="hero-action">
-        <div className="hero-connect">Connect With Me</div>
+        <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
         <div className="hero-resume">My Resume</div>
       </div>
     </div>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.svg'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
       <img src={logo} alt="Logo Dev w/Chris" />
       <ul className="nav-menu">
-        <li>Home</li>
-        <li>About Me</li>
-        <li>Services</li>
-        <li>Portfolio</li>
-        <li>Contact</li>
+        <li><AnchorLink className='anchor-link' offset={50} href='#home'><p>Home</p></AnchorLink></li>
+        <li><AnchorLink className='anchor-link' offset={50} href='#about'><p>About Me</p></AnchorLink></li>
+        <li><AnchorLink className='anchor-link' offset={50} href='#services'><p>Services</p></AnchorLink></li>
+        <li><AnchorLink className='anchor-link' offset={50} href='#work'><p>Portfolio</p></AnchorLink></li>
+        <li><AnchorLink className='anchor-link' offset={50} href='#contact'><p>Contact</p></AnchorLink></li>
       </ul>
-      <div className="nav-connect">Connect With Me</div>
+      <div className="nav-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
     </div>
   )
 }
